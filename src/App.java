d132995a27a81469291ee1b88bf90e655e18c982
NathalyC <carlosamanathaly254@gmail.com>
2026-03-26 18:11:05 -0500

    Crear objetos de prueba

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Telefono f1= new Telefono("16 Pro", "iPhone", 900);
+        Telefono f2= new Telefono("Tecno Spark 8C", "Tecno", 150);
+
+        f1.mostrarInfo();
+        f2.mostrarInfo();
     }
 }
