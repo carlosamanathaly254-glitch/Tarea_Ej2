8d241160c8a8bb23db31d95026ab1e7f6a718415
NathalyC <carlosamanathaly254@gmail.com>
2026-03-26 17:54:27 -0500

    Avanzar getters & setters

---
+++ src/Telefono.java
@@ -0,0 +1,37 @@
+/*
+Ejercicio 2 — Clase Telefono
+Autor: Nathaly Carlosama
+
+Enunciado: 
+    Crea una clase llamada Telefono que represente un teléfono.
+*/
+
+public class Telefono {
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca= marca;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo= modelo;
+    }
+
+    public double getPrecio(){
+        return this.precio;
+    }
+    
+    public void setPrecio(){
+        this.precio= precio;
+    }
+}
